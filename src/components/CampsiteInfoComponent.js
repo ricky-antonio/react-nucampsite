484f1601e41495513d186022c70c2789d8e21925
Ricardo Monterrosa <rickyantonio117@gmail.com>
2021-03-23 20:35:01 -0700

    Combining Reducers

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Label, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
-
+const required = val => val;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
 function RenderCampsite({ campsite }) {
     return (
@@ -33,10 +36,119 @@ function RenderComments({ comments }) {
                     )
                 })
                 }
+                <CommentForm />
             </div>
         )
     }
     return <div />
+}
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        }
+
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        console.log("Current state is: " + JSON.stringify(values));
+        alert("Current state is: " + JSON.stringify(values));
+    }
+
+    render() {
+        return (
+            <>
+                <Button outline onClick={this.toggleModal}>
+                    <i className="fa fa-pencil fa-lg" />
+                    Submit Comment
+                </Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <div className="form-group">
+                                <Label htmlFor="rating" >rating</Label>
+                                <Control.select
+                                        model=".rating"
+                                        id="rating"
+                                        name="rating"
+                                        className="form-control"
+                                        validators={{
+                                            required
+                                        }}
+                                    >
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                    <Errors
+                                        className="text-danger"
+                                        model=".rating"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: 'Required'
+                                        }}
+                                    />
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="author" >Your Name</Label>
+                                <Control.text
+                                        model=".author"
+                                        id="author"
+                                        name="author"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Mist be 15 characters or less'
+                                        }}
+                                    />
+                                
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="text" >Comment</Label>
+                                <Control.textarea
+                                        model=".text"
+                                        id="text"
+                                        name="text"
+                                        rows="6"
+                                        className="form-control"
+                                    />
+                            </div>
+                            <Button type="submit" value="submit" color="primary" >Submit</Button>
+
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+
+            </>
+
+        )
+    }
 }
 
 
@@ -59,7 +171,9 @@ function CampsiteInfo(props) {
                 <div className="row">
                     <RenderCampsite campsite={campsite} />
                     <RenderComments comments={comments} />
+
                 </div>
+
             </div>
         )
     }
